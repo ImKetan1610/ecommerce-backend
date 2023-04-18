8b4503ebcc21b0519c6cf6ab152d5f1e16986030
ImKetan1610 <ketan.makode@gmail.com>
2023-04-18 15:31:37 +0530

    updated: refresh token function to user controllers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,6 +3,7 @@ const User = require("../models/user.model");
 const asyncHandler = require("express-async-handler");
 const { validateMongodbId } = require("../utils/validateMongodbId");
 const { generateRefreshToken } = require("../config/refreshtoken");
+const jwt = require("jsonwebtoken");
 
 const createUser = asyncHandler(async (req, res) => {
   const { email } = req.body;
@@ -49,6 +50,23 @@ const loginUserControl = asyncHandler(async (req, res) => {
   } else {
     throw new Error("Invalid Credentials");
   }
+});
+
+// handle refresh token
+
+const handleRefreshToken = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  console.log(cookie);
+  if (!cookie?.refreshToken) throw new Error("No Refresh Token in Cookies");
+  const refreshToken = cookie?.refreshToken;
+  console.log(refreshToken);
+  const user = await User.findOne({ refreshToken });
+  if (!user) throw new Error("no token is present in db or no matched");
+  jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
+    if(err || user.id !== decoded.id){ throw new Error("Something is wrong with the refresh Token")}
+    const accessToken = generateToken(user.id)
+    res.status(200).send({"accessToken":accessToken});
+  });
 });
 
 // Get All Users
@@ -155,4 +173,5 @@ module.exports = {
   updateAUser,
   blockUser,
   unBlockUser,
+  handleRefreshToken,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,7 @@ const {
   updateAUser,
   blockUser,
   unBlockUser,
+  handleRefreshToken,
 } = require("../controller/user.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -15,6 +16,8 @@ const router = express.Router();
 router.post("/register", createUser);
 router.post("/login", loginUserControl);
 router.get("/all-users", getAllUsers);
+router.get("/refresh", handleRefreshToken);
+
 router.get("/:id", authMiddleware, isAdmin, getAUser);
 router.delete("/:id", deleteAUser);
 //after adding authmiddleware
@@ -22,5 +25,6 @@ router.delete("/:id", deleteAUser);
 router.put("/edit-user", authMiddleware, updateAUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unBlockUser);
+
 
 module.exports = router;
